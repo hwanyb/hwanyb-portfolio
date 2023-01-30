93e896e35deb3a4737b0932bd8bbd7fe2feadeb6
hwanyb <mnn9502@naver.com>
2023-01-30 15:00:00 +0900

    Feat : ThemeProvider 바인딩 및 로컬스토리지 모드 변경 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
+
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { darkTheme, lightTheme } from "./styles/theme";
+import { RootState } from "./modules";
 
 function App() {
+  const isDarkMode = useSelector(
+    (state: RootState) => state.modeReducer.isDarkMode,
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
+  }, [isDarkMode]);
+
   return (
-<h2>test</h2>
-    );
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <h2>test</h2>
+    </ThemeProvider>
+  );
 }
 
 export default App;
