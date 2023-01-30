62516b440e2ae87d9edfd19478c5b873aacb746e
hwanyb <mnn9502@naver.com>
2023-01-30 18:00:00 +0900

    Design : Layout 컴포넌트 반응형 스타일링

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -1,9 +1,32 @@
-import React, { ReactNode } from "react";
+import React from "react";
 import Header from "./Header";
 import styled from "styled-components";
 
-const Base = styled.div``;
-const Main = styled.main``;
+const Base = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 0 200px;
+  @media ${(props) => props.theme.windowSize.desktop} {
+    padding: 0 150px;
+  }
+  @media ${(props) => props.theme.windowSize.laptop} {
+    padding: 0 100px;
+  }
+  @media ${(props) => props.theme.windowSize.tablet} {
+    padding: 0 50px;
+  }
+  @media ${(props) => props.theme.windowSize.mobile} {
+    padding: 0 20px;
+  }
+`;
+const Main = styled.main`
+  width: 100%;
+  height: calc(100% - 100px);
+  @media ${(props) => props.theme.windowSize.tablet} {
+    height: calc(100% - 150px);
+
+  }
+`;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
