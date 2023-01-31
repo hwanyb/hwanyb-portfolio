29094031b8135e3a21e043314a8f4f560e613f58
hwanyb <mnn9502@naver.com>
2023-01-31 16:00:00 +0900

    Design : About 페이지 스타일링

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,42 +7,174 @@ import cerficationData from "../data/certification.json";
 import frontendSkillData from "../data/frontendSkill.json";
 import etcSkillData from "../data/etcSkill.json";
 
-const Base = styled.div``;
-const ProfileWrapper = styled.div``;
-const ProfileImg = styled.div``;
-const NameWrapper = styled.div``;
+const Base = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding-bottom: 50px;
+`;
+const ProfileWrapper = styled.div`
+  margin-right: 4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const ProfileImg = styled.div`
+  border-radius: 150px;
+  width: 300px;
+  height: 400px;
+  background-image: url(${process.env.PUBLIC_URL}/assets/profileImg.jpg);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: ${(props) => props.theme.color.borderColor};
+`;
+const NameWrapper = styled.div`
+  margin-bottom: -4rem;
+  & h1 {
+    font-size: 4rem;
+    font-weight: 900;
+  }
+  & h4 {
+    font-size: ${(props) => props.theme.fontSize.base};
+    font-weight: 300;
+    margin-top: 1rem;
+  }
+`;
 const ProfileDetail = styled.ul``;
 const ProfileItem = styled.li`
   display: flex;
+  line-height: 2rem;
 `;
-const ProfileLabel = styled.p``;
-const ProfileContent = styled.p``;
+const ProfileLabel = styled.p`
+  width: 100px;
+  font-size: ${(props) => props.theme.fontSize.base};
+  display: flex;
+  align-items: center;
+  font-weight: 300;
+  &::before {
+    content: "";
+    width: 3px;
+    height: 1rem;
+    background-color: ${(props) => props.theme.color.textColor};
+    display: inline-block;
+    margin-right: 10px;
+  }
+`;
+const ProfileContent = styled.p`
+  font-size: ${(props) => props.theme.fontSize.base};
+  font-weight: 700;
+`;
 
-const DetailContainer = styled.div``;
-const DetailWrapper = styled.ul``;
-const Title = styled.h2``;
+const DetailContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  & > div {
+    width: 100%;
+  }
+`;
+const DetailWrapper = styled.ul`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+const Title = styled.h2`
+  font-size: ${(props) => props.theme.fontSize.lg};
+  font-weight: 900;
+  margin-bottom: 0.5rem;
+`;
 const DetailItem = styled.li``;
-const Label = styled.label``;
-const SubTitle = styled.h3``;
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+  line-height: 2.5rem;
+  font-size: ${(props) => props.theme.fontSize.md};
+  font-weight: 700;
+  &::before {
+    content: "";
+    width: 3px;
+    height: 1rem;
+    background-color: ${(props) => props.theme.color.textColor};
+    display: inline-block;
+    margin-right: 10px;
+  }
+`;
+const SubTitle = styled.h3`
+  font-size: ${(props) => props.theme.fontSize.base};
+  font-weight: 600;
+`;
 const Desc = styled.p`
   white-space: pre;
+  font-size: ${(props) => props.theme.fontSize.sm};
+  font-weight: 300;
+  margin-top: 10px;
 `;
 
 const SkillDesc = styled.p`
   position: absolute;
+  font-size: ${(props) => props.theme.fontSize.sm};
+  font-weight: 300;
   display: none;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  padding: 0 2rem;
+  background-color: ${props => props.theme.color.bgColor};
+  backdrop-filter: blur(100px);
+  -webkit-backdrop-filter: blur(100px);
+  border-radius: 30px;
+  text-align: center;
+  flex-direction: column;
+  justify-content: center;
+  & b {
+  font-weight: 600;
+
+  }
 `;
-const SkillWrapper = styled.div``;
+
+const SkillContainer = styled.div`
+  width: 100%;
+  margin-top: 1rem;
+`;
+const SkillWrapper = styled.div`
+  position: relative;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 30px;
+  backdrop-filter: blur(25px);
+  -webkit-backdrop-filter: blur(25px);
+  box-shadow: 0 0 5px rgba(9, 9, 9, 0.1);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+  margin-bottom: 1.5rem;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  & ${Desc} {
+    position: absolute;
+    top: -1rem;
+  }
+`;
 const SkillItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 0.5rem;
   @media (hover: hover) {
     &:hover {
       & ${SkillDesc} {
-        display: block;
+        display: flex;
       }
     }
   }
 `;
-const SkillText = styled.p``;
 
 export default function About() {
   return (
@@ -64,65 +196,67 @@ export default function About() {
       </ProfileWrapper>
 
       <DetailContainer>
-        <DetailWrapper>
+        <div>
           <Title>Education.</Title>
-          {educationData.map((item) => (
-            <DetailItem key={item.id}>
-              <Label>{item.duration}</Label>
-              <SubTitle>{item.title}</SubTitle>
-              <Desc>{item.desc}</Desc>
-            </DetailItem>
-          ))}
-        </DetailWrapper>
-
-        <DetailWrapper>
+          <DetailWrapper>
+            {educationData.map((item) => (
+              <DetailItem key={item.id}>
+                <Label>{item.duration}</Label>
+                <SubTitle>{item.title}</SubTitle>
+                <Desc>{item.desc}</Desc>
+              </DetailItem>
+            ))}
+          </DetailWrapper>
+        </div>
+        <div>
           <Title>Certificarion.</Title>
-          {cerficationData.map((item) => (
-            <DetailItem key={item.id}>
-              <Label>{item.title}</Label>
-              <Desc>{item.date}</Desc>
-              <Desc>{item.desc}</Desc>
-            </DetailItem>
-          ))}
-        </DetailWrapper>
-
-        <DetailWrapper>
+          <DetailWrapper>
+            {cerficationData.map((item) => (
+              <DetailItem key={item.id}>
+                <Label>{item.title}</Label>
+                <Desc>{item.date}</Desc>
+                <Desc>{item.desc}</Desc>
+              </DetailItem>
+            ))}
+          </DetailWrapper>
+        </div>
+        <div>
           <Title>Skill Set.</Title>
-          <SkillWrapper>
-            <Desc>Front-End</Desc>
-            {frontendSkillData.map((item) => (
-              <SkillItem key={item.id}>
-                <svg
-                  role="img"
-                  viewBox="0 0 30 30"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                >
-                  <path d={item.icon} fill={item.color}></path>
-                </svg>
-                <SkillText>{item.title}</SkillText>
-                <SkillDesc>{item.desc}</SkillDesc>
-              </SkillItem>
-            ))}
-          </SkillWrapper>
-          <SkillWrapper>
-            <Desc>ETC.</Desc>
-            {etcSkillData.map((item) => (
-              <SkillItem key={item.id}>
-                <svg
-                  role="img"
-                  viewBox="0 0 30 30"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="30"
-                >
-                  <path d={item.icon} fill={item.color}></path>
-                </svg>
-                <SkillText>{item.title}</SkillText>
-                <SkillDesc>{item.desc}</SkillDesc>
-              </SkillItem>
-            ))}
-          </SkillWrapper>
-        </DetailWrapper>
+          <SkillContainer>
+            <SkillWrapper>
+              <Desc>Front-End</Desc>
+              {frontendSkillData.map((item) => (
+                <SkillItem key={item.id}>
+                  <svg
+                    role="img"
+                    viewBox="0 0 30 30"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                  >
+                    <path d={item.icon} fill={item.color}></path>
+                  </svg>
+                  <SkillDesc><b>{item.title}</b><br />{item.desc}</SkillDesc>
+                </SkillItem>
+              ))}
+            </SkillWrapper>
+            <SkillWrapper>
+              <Desc>ETC.</Desc>
+              {etcSkillData.map((item) => (
+                <SkillItem key={item.id}>
+                  <svg
+                    role="img"
+                    viewBox="0 0 30 30"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="30"
+                  >
+                    <path d={item.icon} fill={item.color}></path>
+                  </svg>
+                  <SkillDesc><b>{item.title}</b><br />{item.desc}</SkillDesc>
+                </SkillItem>
+              ))}
+            </SkillWrapper>
+          </SkillContainer>
+        </div>
       </DetailContainer>
     </Base>
   );
